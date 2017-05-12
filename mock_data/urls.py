e676bff4d7a69cc29c86682ecc1ef3678977ee6b
han <rui19921122@qq.com>
2017-05-12 16:40:31 +0800

    增加了一些模拟数据

--- mock_data/urls.py
+++ mock_data/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^mock-plan-data/', views.get_mock_plan_data),
     url(r'^mock-history-data/', views.get_mock_history_data),
+    url(r'^mock-detail-data/(\d{1,9})', views.get_mock_detail_data),
 ]
--- mock_data/views.py
+++ mock_data/views.py
@@ -1,4 +1,7 @@
+import os
 import time
+
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -21,3 +24,14 @@ def get_mock_history_data(request):
     with open(r'./mock_data/global.json', 'r', encoding='utf8') as history:
         history_data = json.load(history)
     return Response(history_data)
+
+
+@api_view(["GET"])
+def get_mock_detail_data(request, wx_id):
+    path = os.path.join('./mock_data/data', '{}.json'.format(wx_id))
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf8') as detail:
+            detail_data = json.load(detail)
+            return Response(detail_data)
+    else:
+        raise Http404
