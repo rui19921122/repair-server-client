5833653fc464c8af7add5c5ca1c5519e20e00010
han <rui19921122@qq.com>
2017-08-31 14:45:10 +0800

    增加了一些模拟数据

--- scrapy_history_list/views.py
+++ scrapy_history_list/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 import datetime
 from rest_framework import generics, permissions
-from scrapy_func import get_history_list
+from rest_framework.response import Response
+
+from config import is_in_rail_net
+from scrapy_func.get_history_list import get_repair_plan_list, generate_repair_plan_list
 
 
 # Create your views here.
 
 class ScrapyHistoryListView(generics.RetrieveAPIView):
-    # permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def retrieve(self, request, *args, **kwargs):
-        print(self.request.query_params)
         try:
             start_date = datetime.datetime.strptime(self.request.query_params.get('start'), '%Y%m%d').date()
             end_date = datetime.datetime.strptime(self.request.query_params.get('end'), '%Y%m%d').date()
@@ -18,4 +20,8 @@ class ScrapyHistoryListView(generics.RetrieveAPIView):
             raise ValueError("提供的参数缺失")
         if end_date < start_date:
             raise ValueError("日期提供错误")
-        print(start_date, end_date)
+        if is_in_rail_net:
+            data = get_repair_plan_list(start_date, end_date)
+        else:
+            data = generate_repair_plan_list(start_date, end_date)
+        return Response(data={'data': data})
