4b443ec9d902f1254a4ebd903194756208424c6f
HanRui <rui19921122@qq.com>
2017-09-04 20:25:31 +0800

    删除了一些傻逼文件

--- repair_scrapy/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-REPAIR_URL_HOST = "REPAIR_URL_HOST"
-REPAIR_DETAIL_URL = "REPAIR_DETAIL_URL"
-
-
-class RepairScrapyConfig(AppConfig):
-    name = 'repair_scrapy'
-    REPAIR_URL_HOST = 'http://10.128.20.119:8080'
